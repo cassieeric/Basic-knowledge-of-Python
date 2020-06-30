e30b686a69e8998f777a0ecbc3392e79b0b0589d
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2020-06-30 21:27:36 +0800

    Add files via upload

---
+++ geShiHua/geshihuaTest.py
@@ -0,0 +1,19 @@
+age = 18
+print("Age is %s" % age)
+print("Age is %03d" % age)
+
+weight = 64.5
+print("His weight is %f kg." % weight)
+print("His weight is %.2f kg." % weight)
+
+
+
+print("His age is %s, weight is %.2f kg." % (age + 1, weight))
+
+stu_id = 2
+print("His age is %s, weight is %.2f kg, stu_id is %06d." % (age, weight, stu_id))
+
+print("His age is %s, weight is %s kg, stu_id is %s." % (age, weight, stu_id))
+
+print(f"His age is {age}, weight is {weight} kg, stu_id is {stu_id}.")
+
